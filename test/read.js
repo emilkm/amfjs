27f24602f4eecf813e63f83559140dc6bbe6172b
emilkm <emilkm@users.noreply.github.com>
2016-10-17 22:30:59 +1300

    add read/write tests for AMF3 null, boolean true, boolean false

--- test/read.js
+++ test/read.js
@@ -3,7 +3,34 @@ var fs = require("fs");
 
 require("../amf");
 
-test("readUTF", function (t) {
+test("readAmf3Null", function (t) {
+  var data = fs.readFileSync("asset/value/null.bin3");
+  var obj = {value: null};
+  var reader = new amf.Reader(data);
+  var res = reader.readObject();
+  t.deepEqual(res, obj);
+  t.end();
+});
+
+test("readAmf3BooleanTrue", function (t) {
+  var data = fs.readFileSync("asset/value/boolean-true.bin3");
+  var obj = {value: true};
+  var reader = new amf.Reader(data);
+  var res = reader.readObject();
+  t.deepEqual(res, obj);
+  t.end();
+});
+
+test("readAmf3BooleanFalse", function (t) {
+  var data = fs.readFileSync("asset/value/boolean-false.bin3");
+  var obj = {value: false};
+  var reader = new amf.Reader(data);
+  var res = reader.readObject();
+  t.deepEqual(res, obj);
+  t.end();
+});
+
+test("readAmf3StringUnicode", function (t) {
   var data = fs.readFileSync("asset/value/string-unicode.bin3");
   var obj = {value: "витоша, 富士山, 珠穆朗瑪峰, आमा"};
   var reader = new amf.Reader(data);
--- test/write.js
+++ test/write.js
@@ -3,7 +3,37 @@ var fs = require("fs");
 
 require("../amf");
 
-test("writeUTF", function (t) {
+test("writeAmf3Null", function (t) {
+  var data = [].slice.call(fs.readFileSync("asset/value/null.bin3"));
+  var obj = {value: null};
+  var writer = new amf.Writer();
+  writer.writeObject(obj);
+  writer.data.unshift(17);
+  t.deepEqual(data, writer.data);
+  t.end();
+});
+
+test("writeAmf3BooleanTrue", function (t) {
+  var data = [].slice.call(fs.readFileSync("asset/value/boolean-true.bin3"));
+  var obj = {value: true};
+  var writer = new amf.Writer();
+  writer.writeObject(obj);
+  writer.data.unshift(17);
+  t.deepEqual(data, writer.data);
+  t.end();
+});
+
+test("writeAmf3BooleanFalse", function (t) {
+  var data = [].slice.call(fs.readFileSync("asset/value/boolean-false.bin3"));
+  var obj = {value: false};
+  var writer = new amf.Writer();
+  writer.writeObject(obj);
+  writer.data.unshift(17);
+  t.deepEqual(data, writer.data);
+  t.end();
+});
+
+test("writeAmf3StringUnicode", function (t) {
   var data = [].slice.call(fs.readFileSync("asset/value/string-unicode.bin3"));
   var obj = {value: "витоша, 富士山, 珠穆朗瑪峰, आमा"};
   var writer = new amf.Writer();
